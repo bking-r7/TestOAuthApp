6f8386118d2e8471377a06d35192476af0da355b
Brianne King <bking@rapid7.com>
2017-11-30 12:39:21 -0800

    Update to use okta

--- src/main/java/com/example/demo/AdminController.java
+++ src/main/java/com/example/demo/AdminController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class AdminController {
     @GetMapping("/admin")
